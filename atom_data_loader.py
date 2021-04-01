4df5764a05f7f5308270d49083f97c4dca0bc281
Brenno Ferreira <brennofunky@gmail.com>
2021-04-01 09:55:34 -0400

    Add files via upload

--- atom_data_loader.py
+++ atom_data_loader.py
@@ -1,59 +1,65 @@
 import json
 import numpy as np
+from pprint import pprint
+
+#For Values not found in JSON file
+DOES_NOT_EXIST = 0.00
 
 #Linear Algebra Solver for Gamma used in previous Code
 def gammaSolver(BDEM1 , BDEM2 , BDEM1M2):
+
+    #Check if heteroatomic
+    if BDEM1 == BDEM2 == BDEM1M2:
+        return 1.0, 1.0
+
+    # Return 0's if no BDEM1M2
+    elif 0 in [BDEM1, BDEM2, BDEM1M2]:
+        return DOES_NOT_EXIST, DOES_NOT_EXIST
+
+
     A = np.array([[BDEM1,BDEM2],[1,1]])
     B = np.array([2*BDEM1M2,2])
     X = np.linalg.solve(A,B)
     (gamma_M1,gamma_M2) = X
-    return round(gamma_M1,3) , round(gamma_M2,3)
+
+    return gamma_M1, gamma_M2
 
 #Function which takes file as input and outputs two dicitonaries
 def dataOpener(jsonfilepath):
-
-    #For Values not found in JSON file
-    DOES_NOT_EXIST = 0.00
 
     #Openingn JSON file that is in the same folder as scratch file
     with open(jsonfilepath) as f:
         data = json.load(f)
 
     #Initializing the Gamma Dictionary
-    gamma = {}
+    gamma = {m: {} for m in data['cebulk']}
+
 
     for M1 in data['cebulk']:
 
-        #Creating a temporary dictionary to append and add onto the gamma dictionary
-        subEle = {}
-
         for M2 in data['cebulk']:
 
-            #check for Heterolytic values
-            if M1 == M2:
-                subEle[M2] = 1.00
+            if M2 in gamma[M1]:
+                continue
 
-            #Check for if the values exist
-            elif M1+M2 in data['bde']:
-                temp , subEle[M2] = gammaSolver(data['bde'][M1+M1],data['bde'][M2+M2],data['bde'][M1+M2])
+            # get the heteroatomic BDE (check both metal orders)
+            bde = data['bde']
+            ab = bde.get(M1 + M2, bde.get(M2 + M1, 0))
 
-            elif M2+M1 in data['bde']:
-                temp, subEle[M2] = gammaSolver(data['bde'][M1 + M1], data['bde'][M2 + M2], data['bde'][M2 + M1])
+            # get homoatomic BDEs
+            aa = bde.get(M1 + M1, 0)
+            bb = bde.get(M2 + M2, 0)
 
-            #For when values do not exist within the JSON File
-            elif M1 + M2 not in data['bde']:
-                subEle[M2] = DOES_NOT_EXIST
-
-            elif M2 + M1 not in data['bde']:
-                subEle[M2] = DOES_NOT_EXIST
-
-        gamma[M1] = subEle
+            # if BDE is not found, ab will be 0
+            # if aa, bb, or ab = 0, gammas will be 0
+            gamma[M1][M2] , gamma[M2][M1] = gammaSolver(aa, bb, ab)
 
     return data['cebulk'],gamma
 
 cebulk , gamma = dataOpener('agaucu_data.json')
 
-print(f'This is the ce_bulk Dictionary : {cebulk}')
-print(f'This is the Gamma Dictionary : {gamma}')
+#print(gammaSolver(3, 3, 3))
 
+pprint(gamma)
+pprint(cebulk)
 
